d43b0d3c6bc6aab19d40ba30d447ec2150de3207
Daniela <danielacardona2203@gmail.com>
2023-05-16 18:50:00 +0200

    Introduce calculation logic for numeric bill input and display the calculated result

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-const btn = document.querySelector(".btn"),
-    tip = document.querySelector(".tip"),
-    total = document.querySelector(".total"),
-    error = document.querySelector(".error");
+const btn = document.querySelector('.btn'),
+  tip = document.querySelector('.tip'),
+  total = document.querySelector('.total'),
+  error = document.querySelector('.error');
 
 //create a funtion to calculate tip
 
-const hideError =() => {
-    setTimeout(() => {
-        error.style.display = "none";
-    }, 5000)
-}
+const hideError = () => {
+  setTimeout(() => {
+    error.style.display = 'none';
+  }, 5000);
+};
 
 const calculateTip = () => {
-    const bill = document.querySelector(".bill").value;
-    const rate = document.querySelector(".rate").value;
+  const bill = document.querySelector('.bill').value;
+  const rate = document.querySelector('.rate').value;
 
-    if (bill === "" || rate == "") {
-        error.style.display = "block";
-        hideError();
+  if (bill === '' || rate == '') {
+    error.style.display = 'block';
+    hideError();
+    //Add a condition that shows an error message when an user enters a value on the bill that isNan
+  } else if (isNaN(bill)) {
+    error.innerHTML = 'Please enter a number';
+    error.style.display = 'block';
+    hideError();
+  } else {
+    let tipAmt = bill * rate;
+    tipAmt = Math.ceil(tipAmt);
+    tip.innerHTML = `Tip: $${tipAmt}`;
+  }
+};
 
-    }
-}
-
-btn.addEventListener("click", calculateTip);
+btn.addEventListener('click', calculateTip);
